ef93a6bd823ca2560b54425d26c611ec391d2f82
Amir Lavasani <amirm.lavasani@gmail.com>
2022-09-03 19:57:12 +0430

    [master] Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Image from './Image';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Image
+      src="https://img9.irna.ir/d/r2/2022/08/23/4/169868889.jpg"
+      alt="a person talking"
+    />
+  </div>
+);
 
 export default App;
