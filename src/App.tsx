a29dae8289c003427b149ad6745f7d5288931209
Amir Lavasani <amirm.lavasani@gmail.com>
2022-09-05 21:39:21 +0430

    [master] Add html img tag attributes as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import './App.css';
 import Image from './inteliver/Image';
+
+const styleImage: CSSProperties = {
+  // maxWidth: '670px',
+  // maxHeight: '680px',
+};
 
 const App = () => (
   <div className="App">
     <Image
       src="https://img9.irna.ir/d/r2/2022/08/23/4/169868889.jpg"
       alt="a person talking"
+      style={styleImage}
+      className="image-test"
+      width="300"
     />
   </div>
 );
--- src/inteliver/Image/Image.tsx
+++ src/inteliver/Image/Image.tsx
@@ -1,78 +1,202 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, CSSProperties, useContext } from 'react';
 import Configs from '../Configs';
+import InteliverContext from '../Context/InteliverContext';
 
 interface ImageProps {
   /**
-   * Image src url
+   * img HTML tag src url attribute. more info at https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-src
    */
   src: string;
 
   /**
-   * Image alt
+   * img HTML tag alt attribute. more info at https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-alt
    */
   alt: string;
+
+  /**
+   * img HTML tag loading attribute. more info at https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-loading
+   */
+  loading?: 'eager' | 'lazy';
+
+  /**
+   * img HTML tag element style attribute
+   */
+  style?: CSSProperties | undefined;
+
+  /**
+   * img HTML tag css classname attribute
+   */
+  className?: string | undefined;
+
+  /**
+   * img HTML tag referrerpolicy attribute. more info at https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-referrerpolicy
+   */
+  referrerPolicy?:
+    | 'no-referrer'
+    | 'no-referrer-when-downgrade'
+    | 'origin'
+    | 'origin-when-cross-origin'
+    | 'unsafe-url';
+
+  /**
+   * img HTML tag width attribute. more info at https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-width
+   */
+  width?: string | undefined;
+
+  /**
+   * img HTML tag height attribute. more info at https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-height
+   */
+  height?: string | undefined;
 }
 
 /**
  * Primary Inteliver Image component
  */
 
-const Image: FunctionComponent<ImageProps> = ({ src, alt }: ImageProps) => {
+const Image: FunctionComponent<ImageProps> = ({
+  src,
+  alt,
+  loading,
+  style,
+  className,
+  referrerPolicy,
+  width,
+  height,
+}: ImageProps) => {
+  const configs = useContext(InteliverContext);
+
   const inteliverBaseURL = Configs.INTELIVER_BASE_URL;
-  const inteliverCloudName = Configs.INTELIVER_CLOUD_NAME;
+  const inteliverCloudName = Configs.INTELIVER_DEFAULT_CLOUD_NAME;
   const intleiverProtocol = Configs.INTELIVER_PROTOCOL;
 
   const optimizeCommandBuilder = (
-    width: number,
+    modifiedWidth: number,
     format: string,
     formatQuality: number
-  ) => `i_w_${width},i_o_resize,i_o_format_${format}_${formatQuality}`;
+  ) => `i_w_${modifiedWidth},i_o_resize,i_o_format_${format}_${formatQuality}`;
 
   const buildInteliverURL = (commands: string) =>
     `${inteliverBaseURL}/${inteliverCloudName}/${commands}/${intleiverProtocol}/${src}`;
 
-  const imageWidthResolutions = Configs.IMAGE_WIDTH_33VW;
-  const resolutionMultiplier = Configs.IMAGE_WIDTH_33VW_MULTIPLIER;
+  // const imageWidthResolutions = Configs.IMAGE_WIDTH_100VW;
+  // const resolutionMultiplier = Configs.IMAGE_WIDTH_100VW_MULTIPLIER;
+
+  const buildSrcSetByResolutionArray = (
+    widthsObject: { [s: string]: number },
+    format: string,
+    formatQuality: number
+  ) => {
+    let srcSet = '';
+    Object.entries(widthsObject).map(([, pixel]) => {
+      const srcurl = buildInteliverURL(
+        optimizeCommandBuilder(pixel, format, formatQuality)
+      );
+      srcSet += `${srcurl} ${pixel}w, `;
+      return srcurl;
+    });
+    return srcSet;
+  };
 
   const pictureElement = (
     <picture>
       {/* WEBP format for all resoluions */}
-      {imageWidthResolutions.map((imageWidth) => (
-        <source
-          media={`(max-width: ${imageWidth * resolutionMultiplier}px)`}
-          srcSet={buildInteliverURL(
-            optimizeCommandBuilder(
-              imageWidth,
+      {Object.entries(Configs.RESOLUTION_IMAGE_WIDTH).map(
+        ([resoluion, widths]) => (
+          <source
+            media={`(max-width: ${resoluion}px)`}
+            srcSet={buildSrcSetByResolutionArray(
+              widths,
               'webp',
               Configs.OPTIMIZATION_WEBP_QUALITY
-            )
-          )}
-          type="image/webp"
-          key={`${imageWidth}_webp`}
-        />
-      ))}
+            )}
+            type="image/webp"
+            key={`${resoluion}_webp`}
+          />
+        )
+      )}
       {/* JPEG format for all resoluions */}
-      {imageWidthResolutions.map((imageWidth) => (
-        <source
-          media={`(max-width: ${imageWidth * resolutionMultiplier}px)`}
-          srcSet={buildInteliverURL(
-            optimizeCommandBuilder(
-              imageWidth,
+      {Object.entries(Configs.RESOLUTION_IMAGE_WIDTH).map(
+        ([resoluion, widths]) => (
+          <source
+            media={`(max-width: ${resoluion}px)`}
+            srcSet={buildSrcSetByResolutionArray(
+              widths,
               'jpeg',
               Configs.OPTIMIZATION_JPEG_QUALITY
-            )
-          )}
-          type="image/jpeg"
-          key={`${imageWidth}_jpeg`}
-        />
-      ))}
-      <img src={src} alt={alt} />
+            )}
+            type="image/jpeg"
+            key={`${resoluion}_jpeg`}
+          />
+        )
+      )}
+      <img
+        src={src}
+        alt={alt}
+        loading={loading}
+        style={style}
+        referrerPolicy={referrerPolicy}
+        width={width}
+        height={height}
+        className={className}
+      />
     </picture>
   );
+
+  // const pictureElement2 = (
+  //   <picture>
+  //     {/* WEBP format for all resoluions */}
+  //     {imageWidthResolutions.map((imageWidth) => (
+  //       <source
+  //         media={`(max-width: ${imageWidth * resolutionMultiplier}px)`}
+  //         srcSet={buildInteliverURL(
+  //           optimizeCommandBuilder(
+  //             imageWidth,
+  //             'webp',
+  //             Configs.OPTIMIZATION_WEBP_QUALITY
+  //           )
+  //         )}
+  //         type="image/webp"
+  //         key={`${imageWidth}_webp`}
+  //       />
+  //     ))}
+  //     {/* JPEG format for all resoluions */}
+  //     {imageWidthResolutions.map((imageWidth) => (
+  //       <source
+  //         media={`(max-width: ${imageWidth * resolutionMultiplier}px)`}
+  //         srcSet={buildInteliverURL(
+  //           optimizeCommandBuilder(
+  //             imageWidth,
+  //             'jpeg',
+  //             Configs.OPTIMIZATION_JPEG_QUALITY
+  //           )
+  //         )}
+  //         type="image/jpeg"
+  //         key={`${imageWidth}_jpeg`}
+  //       />
+  //     ))}
+  //     <img
+  //       src={src}
+  //       alt={alt}
+  //       loading={loading}
+  //       style={style}
+  //       referrerPolicy={referrerPolicy}
+  //       width={width}
+  //       height={height}
+  //       className={className}
+  //     />
+  //   </picture>
+  // );
 
   return pictureElement;
 };
 
-Image.defaultProps = {};
+Image.defaultProps = {
+  loading: 'eager',
+  style: undefined,
+  referrerPolicy: 'origin',
+  width: undefined,
+  height: undefined,
+  className: undefined,
+};
 
 export default Image;
